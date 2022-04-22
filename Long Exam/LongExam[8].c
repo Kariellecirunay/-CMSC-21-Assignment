5346dc39ac89d9af3c423ffc1b96c2f36df48bcb
Kariellecirunay <99890275+Kariellecirunay@users.noreply.github.com>
2022-04-22 15:43:21 +0800

    Add files via upload

---
+++ Long Exam/LongExam[8].c
@@ -0,0 +1,17 @@
+/*Preprocessor directive to include the library */
+#include <stdio.h>
+
+int main(){
+
+    int a,b; /* declaring a and b as variables with int type */
+
+    /*if and else, wherein if statement will work if true, but if false then else statement will work */
+    if ( b == 3 && a == 2 ){
+        printf("-----");}
+    else
+        printf("a.*****\n >>>>>\n<<<<<\n"); /*print the following statements */
+        printf("b.*****\n");
+        printf("c.*****\n <<<<<");
+
+    return 0;
+}
